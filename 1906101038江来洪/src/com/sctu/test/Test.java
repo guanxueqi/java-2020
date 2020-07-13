d5ab2a7e1e1247f1c8298d173dda9618ff288ab7
jianglaihong1 <2060736540@qq.com>
2020-07-13 10:57:57 +0800

    test

--- 1906101038江来洪/src/com/sctu/test/Test.java
+++ 1906101038江来洪/src/com/sctu/test/Test.java
@@ -2,6 +2,6 @@ package com.sctu.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello,world");
+        System.out.println("Hello,world!");
     }
 }
