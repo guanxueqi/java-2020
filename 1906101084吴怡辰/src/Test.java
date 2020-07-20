be8b05bf6b955fecdc213bd50a8475597a5648ff
wuyichen1 <1532409005@qq.com>
2020-07-20 13:12:10 +0800

    test

--- 1906101084吴怡辰/src/Test.java
+++ 1906101084吴怡辰/src/Test.java
@@ -1,7 +1,5 @@
-package Test;
-
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello,world");
+    public static void main(String[] args){
+        System.out.println("Hello,World");
     }
 }
