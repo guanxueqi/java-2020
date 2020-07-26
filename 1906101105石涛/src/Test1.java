1f8e66aa9e2ddf7f86183c9d7f025bbc94f80b10
st13739438729 <1837211345@qq.com>
2020-07-27 00:10:25 +0800

    change

--- 1906101105石涛/src/Test1.java
+++
@@ -1,9 +0,0 @@
-// 练习：求两个数的和
-public class Test1 {
-    public static void main(String[] args){
-        System.out.println(sum(5,7));
-    }
-    public static int sum(int a, int b){
-        return a+b;
-    }
-}
